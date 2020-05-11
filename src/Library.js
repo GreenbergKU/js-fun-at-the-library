64b92c709c93bc9325bff8a68e24c40ad9aed812
Suzie Q <suzieqwolf@gmail.com>
2020-05-10 20:01:42 -0600

    Changes to checkoutBook(), adds clarity.

--- src/Library.js
+++ src/Library.js
@@ -6,7 +6,7 @@ function createLibrary(name) {
       fiction: [],
       nonFiction: [],
     },
-  }
+  };
   return library
 }
 
@@ -17,41 +17,34 @@ function addBook(library, book) {
 }
 
 function checkoutBook(library, title) {
-  var index, shelfGenres = [
-      library.shelves.fantasy,
-      library.shelves.nonFiction, 
-      library.shelves.fiction
+  var x, shelfGenres = [
+    library.shelves.fantasy,
+    library.shelves.nonFiction, 
+    library.shelves.fiction,
   ];
   var checkOutYes = `You have now checked out ${title} from the ${library.name}`;
   var checkOutNo = `Sorry, there are currently no copies of ${title} available at the ${library.name}`;
   for (var i = 0; i < shelfGenres.length; i++) {
-      index = shelfGenres[i].findIndex(function(book) {
-          return book.title === title
-      }); 
-      index !== -1 ? shelfGenres[i].splice(index, 1) : shelfGenres[i];
+    x = shelfGenres[i].findIndex(function(book) { 
+      return book.title === title
+    })
+    shelfGenres[i].splice(x, x !== -1 ? 1 : 0); 
   };
-  return index !== -1 ? checkOutYes : checkOutNo;
+  
+return x !== -1 ? checkOutYes : checkOutNo
 }
 
-//////// CONSOLE.LOG("checkoutBook(library, title)") /////////////
-// console.log(shelfGenres);
-// console.log(library.shelves)
-// console.log("a1:", book.genre, "a2:", typeof(book.Genre), 'a3:', library.shelves[`${bookGenre}`])
-// console.log("A", library.shelves);
-//console.log("1:", shelfGenres[2])
-//console.log("2:", shelfGenres[i]);   
-//console.log("3:", shelfGenres)
-//console.log("B:", library.shelves)
-  
-  //***************************************************//
-    ////  UNSHELFBOOK() from SHELF.js //// (b/c seemed so similar)
-    //****************************************************//
-    // function unshelfBook(title, shelf) {
-    //   shelf.splice(shelf.findIndex(function(book) {
-    //     return book.title === title
-    //   }), 1);
-    // }
-    //**************************************************//
+
+//   var checkOutYes = `You have now checked out ${title} from the ${library.name}`;
+//   var checkOutNo = `Sorry, there are currently no copies of ${title} available at the ${library.name}`;
+//   for (var i = 0; i < shelfGenres.length; i++) {
+//     shelfGenres[i].splice(x, 
+//       (x = shelfGenres[i].findIndex(function(book) { 
+//       return book.title === title
+//     })) !== -1 ? 1 : 0)  
+//   };
+//   return x !== -1 ? checkOutYes : checkOutNo
+// } 
 
 module.exports = {
   createLibrary: createLibrary,
