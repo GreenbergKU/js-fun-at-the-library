6f4d598b50b98f91b5dc5eebdeb483c1e998ae89
Suzie Q <suzieqwolf@gmail.com>
2020-05-15 12:16:01 -0600

    Instructor suggestions trial

--- src/librarian.js
+++ src/librarian.js
@@ -1,3 +1,6 @@
+var checkoutBook = require('../src/Library.js').checkoutBook;
+var searchShelf = require('../src/shelf.js').searchShelf;
+
 class Librarian {
   constructor(name, library, morning) {
     this.name = name;
@@ -9,14 +12,27 @@ class Librarian {
     return morning ? `Good morning, ${name}!`:`Hello, ${name}!`;
   }
 
-  findBook(title) { 
-    var fantasyShelf = this.library.shelves.fantasy;
-    if (fantasyShelf[0].title === title) {
-      fantasyShelf.pop(); 
-      return `Yes, we have ${title}`
-    };
-    return `Sorry, we do not have ${title}`
+  // findBook(title) { 
+  //   var fantasyShelf = this.library.shelves.fantasy;
+  //   if (fantasyShelf[0].title === title) {
+  //     fantasyShelf.pop(); 
+  //     return `Yes, we have ${title}`
+  //   };
+  //   return `Sorry, we do not have ${title}`
+  // }
+  findBook(title) {
+    console.log("A", "B", checkoutBook(this.library, title));
+    ///if (searchShelf(this.shelf, title) == false ) {
+      //
+
+      if (checkoutBook(this.library, title) == 
+        `Sorry, there are currently no copies of ${title} available at the ${this.library}`) {
+          return `Sorry, we do not have ${title}`
+        } else {
+        return `Yes, we have ${title}` 
+        }
   }
+
   
   calculateLateFee(daysPastDue) {
     return Math.ceil(daysPastDue * .25)
